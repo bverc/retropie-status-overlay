217ca384b75a094301806af5b4358e29fd3f86b3
kineticmedic <simon@thecheesemustflow.com>
2026-05-28 04:42:13 -0400

    Create pisugar.py (#39)
    This will pull the voltage information over I2C for a PiSugar3 Plus. Tested and confirmed on my own hardware.
    
    Co-authored-by: Brendan Vercoelen <bvercoelen@gmail.com>

--- adc/pisugar.py
+++ adc/pisugar.py
@@ -19,4 +19,7 @@ bus = smbus.SMBus(1)
 
 # ADC read function, return voltage
 def read(channel):
-  return bus.read_word_data(address, 0x22) / 1000
+  raw = bus.read_word_data(address, 0x22)
+  raw_swapped = ((raw & 0xFF) << 8) | ((raw >> 8) & 0xFF)
+  voltage = raw_swapped / 1000.0
+  return voltage
